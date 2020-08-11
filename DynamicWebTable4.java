93a539f9b2c1d1837056a85a3b1365bbe8648c80
QAautomation20 <66098551+QAautomation20@users.noreply.github.com>
2020-08-10 22:10:51 -0500

    Add files via upload
    forgot to commit earlier

---
+++ DynamicWebTable4.java
@@ -0,0 +1,105 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DynamicWebTable4 {
+
+		WebDriver driver;
+
+		@BeforeMethod
+		public void launchBrowser() throws Exception {
+			System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //60 seconds is industry standard
+			driver.get("http://techfios.com/test/billing/?ng=login/");
+		}
+
+		@Test
+		public void learnDynamicWebTable() throws InterruptedException {
+			driver.findElement(By.xpath("//input[@type='text']")).sendKeys("techfiosdemo@gmail.com");
+			driver.findElement(By.xpath("//*[contains(@placeholder, 'ssword') and contains(@type, 'password')]")).sendKeys("abc123");
+			driver.findElement(By.xpath("//*[contains(text(), 'ign in') and @name = 'login']")).click();
+			driver.findElement(By.xpath("//ul[@id='side-menu']/descendant::span[text() = 'Bank & Cash' ]")).click();
+
+			//Explicit Wait
+			new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@class='nav nav-second-level collapse in']/descendant::a[text() = 'New Account' ]")));
+			
+		/*
+		 * //Fluent Wait Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+		 * .withTimeout(10, TimeUnit.SECONDS) .pollingEvery(5, TimeUnit.SECONDS)
+		 * .ignoring(NoSuchElementException.class);
+		 */
+				
+			
+			driver.findElement(By.xpath("//ul[@class='nav nav-second-level collapse in']/descendant::a[text() = 'New Account' ]")).click();
+
+			String expectedAccountTitle = "Dynamic Web Table" + new Random().nextInt(999);
+			String Balance = "5,000" + new Random().nextInt(999);
+			
+			System.out.println(expectedAccountTitle);
+
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//input[@id='account']")).sendKeys(expectedAccountTitle);
+			driver.findElement(By.xpath("//div[@class='ibox-content']/descendant::input[@id='description']")).sendKeys("Machhapuchhre");
+			driver.findElement(By.xpath("//div[@class='form-group']/descendant::input[@id='balance']")).sendKeys(Balance);
+			driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+			
+	//	Message Validation
+	//	By ACCOUNT_CREATED_SUCCESSFULLY_LOCATOR = By.xpath("//*[@id='page-wrapper']/div[3]/div[1]/text()[3]");
+	//	String ExpectedMessage = "Account Created Successfully";
+			
+	//	new WebDriverWait (driver,20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='page-wrapper']/div[3]/div[1]/text()[3]")));
+	//	Assert.assertEquals(ACCOUNT_CREATED_SUCCESSFULLY_LOCATOR, ExpectedMessage, "The message does not match");
+
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//span[text() = 'Transactions']")).click();
+		driver.findElement(By.xpath("//a[text() = 'View Transactions']")).click();
+		
+		List<WebElement> ColumnHeaders = driver.findElements(By.xpath("//table/descendant::th"));
+		List<String> HeaderNames = new ArrayList<String>();
+		
+		for (WebElement Headers : ColumnHeaders) {
+			HeaderNames.add(Headers.getText());
+			System.out.println(HeaderNames);
+		}
+	//	for (String Names : HeaderNames) {
+		//	System.out.println(HeaderNames);
+	//	}
+		
+		List<WebElement> ColumnRowsData = driver.findElements(By.xpath("//table/descendant::td"));
+		List<String> AccountNames = new ArrayList<String>();
+		
+		for (WebElement RowsData : ColumnRowsData) {
+			AccountNames.add(RowsData.getText());
+		}
+		for (String ACC : AccountNames) {
+			System.out.println(ACC);
+		}
+		System.out.println("--------------------------------");
+		
+		if(AccountNames.contains(expectedAccountTitle)) {
+			System.out.println("Test Pass");
+		}
+		else
+		{
+			System.out.println("Test Fail");
+		}
+	}
+
+}
